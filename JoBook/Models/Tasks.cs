7329651988c9ecca08d1099728b3bb0cc9466ad7
Ale180820 <aylinneart@gmail.com>
2020-04-04 22:13:21 -0600

    Add Custom generics

--- JoBook/Models/Tasks.cs
+++ JoBook/Models/Tasks.cs
@@ -5,7 +5,17 @@ using System.Web;
 
 namespace JoBook.Models
 {
+     /*
+     * @author: Aylinne Recinos
+     * @version: 1.0.0
+     * @description: Class for Tasks
+     */
     public class Tasks
     {
+        public String name { get; set; }
+        public String description { get; set; }
+        public String project { get; set; }
+        public String priority { get; set; }
+        public String dateOfDelivery { get; set; }
     }
 }
--- JoBook/Models/Users.cs
+++ JoBook/Models/Users.cs
@@ -7,5 +7,14 @@ namespace JoBook.Models
 {
     public class Users
     {
+     /*
+     * @author: Aylinne Recinos
+     * @version: 1.0.0
+     * @description: Class for Users
+     */
+        public int idUser { get; set; }
+        public String name { get; set; }
+        public String lastName { get; set; }
+        public String typeUser { get; set; }
     }
 }
