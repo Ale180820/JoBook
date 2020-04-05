eae8203ebf548194dba07632b85e10a7406f83ba
victooorisimo <vmelendrez2013241@gmail.com>
2020-04-05 11:16:49 -0600

    Add modifications in models

--- JoBook/Models/Tasks.cs
+++ JoBook/Models/Tasks.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace JoBook.Models
-{
-     /*
-     * @author: Aylinne Recinos
-     * @version: 1.0.0
-     * @description: Class for Tasks
-     */
-    public class Tasks
-    {
+/*
+ * @author: Aylinne Recinos
+ * @version: 1.0.0
+ * @description: Class for Tasks
+ */
+
+namespace JoBook.Models {
+
+    public class Tasks {
+
         public String name { get; set; }
         public String description { get; set; }
         public String project { get; set; }
