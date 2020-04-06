d56f1759fdcdded31c4f083299f0419f63d933d9
victooorisimo <vmelendrez2013241@gmail.com>
2020-04-06 01:29:33 -0600

    Login and create user view

---
+++ JoBook/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/*
+* @author: Aylinne Recinos
+* @version: 1.0.0
+* @description: Class for Users
+*/
+
+namespace JoBook.Models {
+
+    public class User {
+
+        public int IdUser { get; set; }
+	    public String Nickname { get; set; }
+        public String Password { get; set; }
+        public String Name { get; set; }
+        public String Lastname { get; set; }
+        public String Type { get; set; }
+    }
+}
