38614675f834c6b2c6c072171a0f47056d74fdae
victooorisimo <vmelendrez2013241@gmail.com>
2020-04-08 20:23:32 -0600

    First commit of hashtable

--- CustomGenerics/Structures/HashTable.cs
+++ CustomGenerics/Structures/HashTable.cs
@@ -1,5 +1,6 @@
 ﻿using CustomGenerics.Interfaces;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,20 +14,53 @@ using System.Threading.Tasks;
 
 
 namespace CustomGenerics.Structures {
-    public class HashTable<T> : IStructures<T>
-    {
-        protected override void Delete(T value)
-        {
+    public class HashTable<T> : IEnumerable<T> {
+
+        private Stack<LinkedList<T>[]> table;
+        private int sizeHashTable;
+
+        public HashTable() {
+            table = new Stack<LinkedList<T>[]>();
+            sizeHashTable++; 
+        }
+
+        public int hashCode(T value){
+            return (20 * value.GetHashCode()) % 5;
+        }
+
+        public bool put(T key, T value) {
+            int hash = hashCode(key);
+            if (table.Peek()[hash] == null) {
+                table.Peek()[hash] = new LinkedList<T>();
+                table.Peek()[hash].AddFirst(value);
+                return true;
+            }else {
+                return false;
+            }
+        }
+
+        public T get(T key){
+            if (table.Count == 0){ return default(T);}
+            try {
+                return table.Peek()[hashCode(key)].First();
+            }catch {
+                return default(T);
+            }
+        }
+
+        public void getAllValues(){
+            for (int i  = 0; i < table.Count; i++){
+                table.Peek()[i].First();
+            }
+        }
+
+        //Interface method
+        IEnumerator IEnumerable.GetEnumerator() {
             throw new NotImplementedException();
         }
 
-        protected override T Get()
-        {
-            throw new NotImplementedException();
-        }
-
-        protected override void Insert(T value)
-        {
+        //Interface method
+        public IEnumerator<T> GetEnumerator() {
             throw new NotImplementedException();
         }
     }
